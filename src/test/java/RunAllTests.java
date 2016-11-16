40a64e9913b6df8452b3c3b997abbb772cf4c4fe
Aliaksei Kirylchyk <akirilchik@exadel.com>
2016-11-16 17:34:10 +0300

    remove unsupported local library

--- src/test/java/RunAllTests.java
+++ src/test/java/RunAllTests.java
@@ -1,12 +1,10 @@
-import com.exadel.pages.ImageEqualityPageTest;
 import com.exadel.pages.HomePageTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(value = org.junit.runners.Suite.class)
 @Suite.SuiteClasses(value = {
-        HomePageTest.class,
-        ImageEqualityPageTest.class
+        HomePageTest.class
 }
 )
 
