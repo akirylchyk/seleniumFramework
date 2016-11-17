12e180a0c9aaf5d19a543d2b83f930af1b51f78a
Aliaksei Kirylchyk <akirilchik@exadel.com>
2016-11-17 11:18:37 +0300

    added new tests for test ng check

--- src/test/java/com/exadel/pages/TestNgTest.java
+++ src/test/java/com/exadel/pages/TestNgTest.java
@@ -1,6 +1,7 @@
 package com.exadel.pages;
 
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.util.logging.Level;
@@ -12,42 +13,54 @@ public class TestNgTest extends TestBase {
 
     @BeforeGroups("shopping")
     public void beforeGroups() {
-        LOGGER.log(Level.ALL, "@BeforeGroups");
+        LOGGER.log(Level.INFO, "@BeforeGroups");
     }
 
     @AfterGroups("shopping")
     public void afterGroups() {
-        LOGGER.log(Level.ALL, "@AfterGroups");
+        LOGGER.log(Level.INFO, "@AfterGroups");
     }
 
     @BeforeClass
     public void beforeClass() {
-        LOGGER.log(Level.ALL, "@BeforeClass");
+        LOGGER.log(Level.INFO, "@BeforeClass");
     }
 
     @AfterClass
     public void afterClass() {
-        LOGGER.log(Level.ALL, "@AfterClass");
+        LOGGER.log(Level.INFO, "@AfterClass");
     }
 
     @BeforeMethod
     public void beforeMethod() {
-        LOGGER.log(Level.ALL, "@BeforeMethod");
+        LOGGER.log(Level.INFO, "@BeforeMethod");
     }
 
     @AfterMethod
     public void afterMethod() {
-        LOGGER.log(Level.ALL, "@AfterMethod");
+        LOGGER.log(Level.INFO, "@AfterMethod");
     }
 
-    @Test(groups = "shopping")
-    public void runTest1() {
-        LOGGER.log(Level.ALL, "@Test - runTest1");
+    @Test(groups = "shopping", timeOut = 5000)
+    public void runTest1() throws InterruptedException {
+        LOGGER.log(Level.INFO, "@Test - runTest1");
+        Assert.assertTrue(true);
+    }
+
+    @Test(dependsOnGroups = "shopping", groups = "not shopping", enabled = true)
+    public void runTest2() {
+        LOGGER.log(Level.INFO, "@Test - runTest2");
+        Assert.assertTrue(true);
+    }
+
+    @Test(dependsOnGroups = "not shopping", groups = "other shopping group", enabled = false)
+    public void runTest3() {
+        LOGGER.log(Level.INFO, "@Test - runTest3");
+        Assert.assertTrue(false);
     }
 
     @Test
-    public void runTest2() {
-        LOGGER.log(Level.ALL, "@Test - runTest2");
+    public void divisionWithException() {
+        int i = 1 / 0;
     }
-
 }
